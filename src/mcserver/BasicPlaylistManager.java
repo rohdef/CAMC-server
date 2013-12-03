c55ffa8f1d5887100dbf006b086e73e71fe45329
U-Esran-PC\Esran <Esran@Esran-PC.(none)>
2013-12-03 22:42:36 +0100

    Bugfixes, tested server a bunch

--- src/mcserver/BasicPlaylistManager.java
+++ src/mcserver/BasicPlaylistManager.java
@@ -1,4 +1,3 @@
-
 package mcserver;
 
 import java.io.File;
@@ -16,12 +15,12 @@ public class BasicPlaylistManager implements Runnable {
     private int currentSong = 0;
     private PlaylistListener listener;
     private static final Logger logger = Logger.getLogger(BasicPlaylistManager.class.getName());
-    
-    static{
+
+    static {
         try {
             File file = new File("logs");
             file.mkdir();
-            FileHandler handler = new FileHandler("logs/"+BasicPlaylistManager.class.getSimpleName()+".log");
+            FileHandler handler = new FileHandler("logs/" + BasicPlaylistManager.class.getSimpleName() + ".log");
             handler.setFormatter(new SimpleFormatter());
             logger.addHandler(handler);
         } catch (IOException ex) {
@@ -30,47 +29,57 @@ public class BasicPlaylistManager implements Runnable {
             logger.log(Level.SEVERE, null, ex);
         }
     }
-    
-    public BasicPlaylistManager(PlaylistListener listener, List<BasicSong> songs){
+
+    public BasicPlaylistManager(PlaylistListener listener, List<BasicSong> songs) {
         playlist = new ArrayList<>();
         playlist.addAll(songs);
         this.listener = listener;
     }
-    
-    public synchronized String getCurrentSong(){
+
+    public synchronized String getCurrentSong() {
         return playlist.get(currentSong).getName();
     }
-    
-    public List<BasicSong> getPlaylist(){
+
+    public List<BasicSong> getPlaylist() {
         ArrayList<BasicSong> res = new ArrayList<>();
         res.addAll(playlist);
         return res;
     }
-    
-    public void run(){
-        for(int i = 0; i < playlist.size(); i++){
-            synchronized(this){
-                currentSong = i;
+
+    /*
+     * Iterates through the playlist, playing the songs as it goes.
+     * When half the last song is done playing, triggers a new voting round.
+     */
+    public void run() {
+        try {
+            if (playlist.isEmpty()) {
+                //No votes received. Wait for 5 seconds, then retrigger voting.
+                Thread.sleep(5000);
+                listener.withinThreshold();
+                return;
             }
-            logger.log(Level.INFO, "Now playing \"" + playlist.get(i).getName() + "\"");
-            int duration = playlist.get(i).getDuration();
-            //Pretend there's code to ask media player to play the song here
-            //Also to remove a song if the media player can't acquire it
-            if(i == playlist.size()-1){
-                try {
-                    //On the last song trigger voting halfway through
-                    Thread.sleep(duration*500);
-                    listener.withinThreshold();
-                    Thread.sleep(duration*500); //This is probably not robust
-                } catch (InterruptedException ex) {
-                    logger.log(Level.SEVERE, null, ex);
+            for (int i = 0; i < playlist.size(); i++) {
+                if(Thread.currentThread().isInterrupted()){
+                    throw new InterruptedException();
                 }
+                synchronized (this) {
+                    currentSong = i;
+                }
+                logger.log(Level.INFO, "Now playing \"" + playlist.get(i).getName() + "\"");
+                int duration = playlist.get(i).getDuration();
+                //Pretend there's code to ask media player to play the song here
+                //Also to remove a song if the media player can't acquire it
+                if (i == playlist.size() - 1) {
+                    //On the last song trigger voting halfway through
+                    Thread.sleep(duration * 500);
+                    listener.withinThreshold();
+                    Thread.sleep(duration * 500); //This is probably not robust
+                }
+                Thread.sleep(duration * 1000);
             }
-            try {
-                Thread.sleep(duration*1000);
-            } catch (InterruptedException ex) {
-                logger.log(Level.SEVERE, null, ex);
-            }
+            logger.log(Level.INFO, "Finished playing playlist");
+        } catch (InterruptedException ex) {
+            logger.log(Level.INFO, "Playlist shutting down");
         }
     }
 }
--- src/mcserver/MCServer.java
+++ src/mcserver/MCServer.java
@@ -1,23 +1,33 @@
 package mcserver;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class MCServer implements PlaylistListener {
 
     public static final int DISCOVERY_PORT = 49155;
-    public static final int VOTING_PORT = 49156;
+    public static final int VOTING_PORT = 50032;
     private Thread discoveryThread = null;
+    private Thread votingThread = null;
+    private Thread votingHandler = null;
     private static final Logger logger = Logger.getLogger(MCServer.class.getName());
     private Voting voting;
 
@@ -27,6 +37,7 @@ public class MCServer implements PlaylistListener {
             file.mkdir();
             FileHandler handler = new FileHandler("logs/" + MCServer.class.getSimpleName() + ".log");
             handler.setFormatter(new SimpleFormatter());
+            handler.setLevel(Level.ALL);
             logger.addHandler(handler);
         } catch (IOException ex) {
             logger.log(Level.SEVERE, "Disabling logging to file", ex);
@@ -36,13 +47,23 @@ public class MCServer implements PlaylistListener {
     }
 
     public static void main(String[] args) throws SocketException, IOException, InterruptedException {
-        MCServer server = new MCServer();
+        final MCServer server = new MCServer();
 
         server.start();
 
-        Thread.sleep(10000);
-
-        server.shutdown();
+        final JFrame frame = new JFrame("Server status");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JButton button = new JButton("Shutdown server");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                server.shutdown();
+                frame.dispose();
+            }
+        });
+        frame.add(button);
+        frame.pack();
+        frame.setVisible(true);
 
     }
 
@@ -51,70 +72,120 @@ public class MCServer implements PlaylistListener {
         voting = new Voting(4);
     }
 
+    //Starts the discovery thread and begins voting
     public void start() {
         discoveryThread.start();
+        try {
+            //For testing on a single machine, client needs time to receive discovery
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+        withinThreshold();
     }
 
+    //Kills all the child threads. Order is important here.
     public void shutdown() {
         logger.log(Level.INFO, "Server shutting down");
+        if (votingHandler != null) {
+            votingHandler.interrupt();
+            try {
+                votingHandler.join();
+            } catch (InterruptedException ex) {
+                logger.log(Level.SEVERE, null, ex);
+            }
+        }
+        voting.shutdown();
+        if (votingThread != null) {
+            votingThread.interrupt();
+        }
         discoveryThread.interrupt();
     }
 
+    //Triggers a new vote
     @Override
     public void withinThreshold() {
-        //Open for voting
-        voting.beginVoting();
-        VotingService votingService = new VotingService();
-        Thread votingThread = new Thread(votingService);
-        votingThread.start();
-        //Broadcast start of voting period
-        try (DatagramSocket socket = new DatagramSocket()) {
-            InetSocketAddress address = new InetSocketAddress("255.255.255.255", VOTING_PORT);
-            byte[] message = new byte[0];
-            socket.setBroadcast(true);
-            DatagramPacket packet = new DatagramPacket(message, message.length);
-            packet.setSocketAddress(address);
-            socket.send(packet);
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, null, e);
+        if (votingHandler != null) {
+            votingHandler.interrupt();
         }
-        try {
-            Thread.sleep(15000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(MCServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        votingThread.interrupt();
-        voting.endVoting(this);
+        votingHandler = new Thread(new VotingHandler());
+        votingHandler.start();
     }
 
-    private class VotingService implements Runnable {
+    public class VotingHandler implements Runnable {
+        /*Broadcasts the message to trigger client voting, runs the Voting code to start a new playlist.
+         * Only a thread so the Playlist manager thread doesn't have to start a new instance of itself through withinThreshold
+         */
 
+        @Override
         public void run() {
-            try (DatagramSocket socket = new DatagramSocket()) {
+            //Open for voting
+            logger.log(Level.INFO, "Beginning voting");
+            voting.beginVoting();
+            VotingReceiver votingService = new VotingReceiver();
+            if (votingThread != null && !votingThread.isInterrupted()) {
+                votingThread.interrupt();
+            }
+            votingThread = new Thread(votingService);
+            votingThread.start();
+            try {
+                /*If client/server are on the same machine, broadcast may arrive at client
+                 * before server opens receive socket. This is not relevant on separate machines
+                 * but it avoids an extra voting cycle on single machine tests.
+                 */
+                Thread.sleep(200);
+                //Broadcast start of voting period
+                try (DatagramSocket socket = new DatagramSocket()) {
+                    InetSocketAddress address = new InetSocketAddress("255.255.255.255", VOTING_PORT);
+                    byte[] message = new byte[0];
+                    socket.setBroadcast(true);
+                    DatagramPacket packet = new DatagramPacket(message, message.length);
+                    packet.setSocketAddress(address);
+                    socket.send(packet);
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, null, e);
+                }
+                Thread.sleep(10000); //Run voting for 10 seconds
+            } catch (InterruptedException ex) {
+                logger.log(Level.INFO, "Voting shutting down");
+            }
+            votingThread.interrupt();
+            voting.endVoting(MCServer.this);
+        }
+    }
+
+    private class VotingReceiver implements Runnable {
+        /*
+         * Receives votes from clients, passes them on to Voting
+         * Notice that this has to happen over TCP instead of UDP to make voting reliable
+         */
+        public void run() {
+            try (ServerSocket serverSocket = new ServerSocket()) {
                 InetSocketAddress address = new InetSocketAddress(InetAddress.getLocalHost(), VOTING_PORT);
-                socket.bind(address);
-                //TODO:Decide if one packet per song is wasteful
-                byte[] message = new byte[1024];
-                DatagramPacket packet = new DatagramPacket(message, message.length);
-                socket.setSoTimeout(1000);
+                serverSocket.bind(address);
+                serverSocket.setSoTimeout(200);
                 while (!Thread.currentThread().isInterrupted()) {
-                    try {
-                        socket.receive(packet);
-                        String name = new String(packet.getData()).trim();
-                        voting.addVote(name);
+                    //TODO: May need to thread this further if client submissions get larger
+                    try (Socket socket = serverSocket.accept()) {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String s = null;
+                        while ((s = reader.readLine()) != null) {
+                            voting.addVote(s);
+                            logger.log(Level.FINE, "Received vote for " + s);
+                        }
                     } catch (SocketTimeoutException e) {
-                        logger.log(Level.FINE, "No votes received in 1 second interval");
+                        logger.log(Level.FINE, "No votes received in 200ms interval");
                     }
                 }
-            } catch (IOException e) {
-                logger.log(Level.SEVERE, null, e);
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, null, ex);
             }
         }
     }
 
     private class DiscoveryService implements Runnable {
 
-        //Broadcasts a packet containing server address to the LAN
+        //Broadcasts a packet containing server address to the LAN every second
         public void run() {
             try (DatagramSocket socket = new DatagramSocket()) {
                 InetSocketAddress address = new InetSocketAddress("255.255.255.255", DISCOVERY_PORT);
